f46c2480a391c9da8aa3349441a1dcbe419e5d95
Jianeng Xu <xujianeng0079@gmail.com>
2019-02-27 12:03:22 -0600

    Update example.py

--- example.py
+++ example.py
@@ -7,12 +7,13 @@ R1 = np.loadtxt("realstock_return.txt")
 R2 = np.loadtxt("realportfolio_return.txt")
 M = np.loadtxt("realMKT.txt")
 T = M.shape[0] # number of periods
-
-# 
-training_para = dict(epoch=50, train_ratio=0.6, split="future", activation=tf.nn.tanh)
-
 data_input = dict(characteristics=Z, stock_return=R1, target_return=R2, factor=M[:, 0:3])
 
-layer_size_tanh = [64, 32, 16, 8, 4]
+# set parameters
+training_para = dict(epoch=50, train_ratio=0.6, split="future", activation=tf.nn.tanh)
 
-dl_alpha(data_input, layer_size_tanh, training_para, value_index=0, ens=100)
+# design network layers
+layer_size = [64, 32, 16, 8, 4]
+
+# construct deep factors
+f, f_oos, alpha, loss = dl_alpha(data_input, layer_size, training_para, value_index=0, ens=100)
